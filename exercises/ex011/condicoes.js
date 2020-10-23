60d566c1ec94bffd4a1a5d61a2f15c0b030dfe05
Juliana Pereira <jotace1234@hotmail.com>
2020-10-23 10:53:44 -0300

    condições done

---
+++ exercises/ex011/condicoes.js
@@ -0,0 +1,7 @@
+var speed = 54
+console.log (`A sua velocidade atual é de ${speed}km/h.`)
+if (speed > 60) {
+    console.log(`Você ultrapassou o limite de velocidade permitido. MULTADO!`)
+} else {
+    console.log (`Você está dentro do limite de velocidade. Dirija sempre com segurança.`)
+}
