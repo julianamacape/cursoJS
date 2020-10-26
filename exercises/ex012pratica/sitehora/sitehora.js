53f88e6fb37806c17bb8b61c6155f36f24633032
Juliana Pereira <jotace1234@hotmail.com>
2020-10-26 18:23:41 -0300

    site hora exata DONE for good!

--- exercises/ex012pratica/sitehora/sitehora.js
+++ exercises/ex012pratica/sitehora/sitehora.js
@@ -11,11 +11,14 @@ function mostrarhora() {
     if (hora >= 0 && hora < 12) {
         picture.src = '01morning.png'
         greeting.innerHTML = 'Bom dia!'
+        document.body.style.background = 'rgba(250, 214, 53, 0.788)'
     } else if (hora <= 18) {
         picture.src = '02afternoon.png'
         greeting.innerHTML = 'Boa Tarde!'
+        document.body.style.background = 'rgba(160, 81, 29, 0.952)'
     } else {
         picture.src = '03night.png'
         greeting.innerHTML = 'Boa noite!'
+        document.body.style.background = 'rgba(9, 6, 51, 0.952)'
     }
 }
