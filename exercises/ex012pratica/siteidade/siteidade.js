3e167b6c27d5d3e459873460d6ce0e1ee065de91
Juliana Pereira <jotace1234@hotmail.com>
2020-10-27 23:00:32 -0300

    siteidade no meio

---
+++ exercises/ex012pratica/siteidade/siteidade.js
@@ -0,0 +1,45 @@
+function Verificar() {
+    var idade = Number(window.document.getElementById('idade').value)
+    var sexo = window.document.getElementById('sexo')
+    var foto = window.document.getElementById('foto')
+    var result = window.document.getElementById('result')
+    
+}
+
+    /* if (sexo == 'Masculino' && idade < 4) {
+        foto.src = 'baby.png'
+        result.innerHTML = `Você é um lindo bebê de ${idade} anos.`
+    } else if (sexo == 'Feminino' && idade < 4) {
+        foto.src = 'baby.png'
+        result.innerHTML = `Você é uma linda bebê de ${idade} anos.`
+    } else if (sexo == 'Masculino' && idade >= 4 && idade < 13) {
+        foto.src = 'childboy.png'
+        result.innerHTML = `Você é um menino de ${idade} anos.`
+    } else if (sexo == 'Feminino' && idade >= 4 && idade < 13) {
+        foto.src = 'childgirl.png'
+        result.innerHTML = `Você é um menina de ${idade} anos.`
+    } else if (sexo == 'Masculino' && idade >= 13 && idade < 19) {
+        foto.src = 'teenboy.png'
+        result.innerHTML = `Você é um adolescente de ${idade} anos.`
+    } else if (sexo == 'Masculino' && idade >= 13 && idade <= 19) {
+        foto.src = 'teengirl.png'
+        result.innerHTML = `Você é uma adolescente de ${idade} anos.`
+    } else if (sexo == 'Masculino' && idade >= 20 && idade < 36) {
+        foto.src = 'adultman.png'
+        result.innerHTML = `Você é um rapaz de ${idade} anos.`
+    } else if (sexo == 'Feminino' && idade >= 20 && idade < 36) {
+        result.innerHTML = `Você é uma moça de ${idade} anos.`
+        foto.src = 'adultwoman.png'
+    } else if (sexo == 'Masculino' && idade >= 36 && idade < 60) {
+        result.innerHTML = `Você é um homem de meia idade de ${idade} anos.`
+        foto.src = 'middleagedman.png'
+    } else if (sexo == 'Feminino' && idade >= 36 && idade < 60) {
+        result.innerHTML = `Você é uma mulher de meia idade de ${idade} anos.`
+        foto.src = 'middleagedwoman.png'
+    } else if (sexo == 'Masculino' && idade >= 60) {
+        result.innerHTML = `Você é um idoso de ${idade} anos.`
+        foto.src = 'elderly.png'
+    } else if (sexo == 'Feminino' && idade >= 60) {
+        result.innerHTML = `Você é uma idosa de ${idade} anos.`
+        foto.src = 'elderly.png'
+    } */
