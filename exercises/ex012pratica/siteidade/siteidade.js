182f5b806ca09bdf45a41d1380dfba30c39b7390
Juliana Pereira <jotace1234@hotmail.com>
2020-10-28 17:43:41 -0300

    siteidade.html done, bitch!

--- exercises/ex012pratica/siteidade/siteidade.js
+++ exercises/ex012pratica/siteidade/siteidade.js
@@ -1,45 +1,43 @@
-function Verificar() {
-    var idade = Number(window.document.getElementById('idade').value)
-    var sexo = window.document.getElementById('sexo')
-    var foto = window.document.getElementById('foto')
-    var result = window.document.getElementById('result')
-    
-}
-
-    /* if (sexo == 'Masculino' && idade < 4) {
+function verificar() {
+    var dob = document.getElementById('txtdob').value
+    var year = new Date().getFullYear()
+    var age = year - dob
+    var clicksexo = document.getElementsByName('gender')
+    if (clicksexo[0].checked && age < 4) {
+        result.innerHTML = `Você é uma linda bebê de ${age} aninhos.`
         foto.src = 'baby.png'
-        result.innerHTML = `Você é um lindo bebê de ${idade} anos.`
-    } else if (sexo == 'Feminino' && idade < 4) {
-        foto.src = 'baby.png'
-        result.innerHTML = `Você é uma linda bebê de ${idade} anos.`
-    } else if (sexo == 'Masculino' && idade >= 4 && idade < 13) {
-        foto.src = 'childboy.png'
-        result.innerHTML = `Você é um menino de ${idade} anos.`
-    } else if (sexo == 'Feminino' && idade >= 4 && idade < 13) {
+    } else if (clicksexo[0].checked && age < 13) {
+        result.innerHTML = `Você é uma linda menina de ${age} aninhos.`
         foto.src = 'childgirl.png'
-        result.innerHTML = `Você é um menina de ${idade} anos.`
-    } else if (sexo == 'Masculino' && idade >= 13 && idade < 19) {
-        foto.src = 'teenboy.png'
-        result.innerHTML = `Você é um adolescente de ${idade} anos.`
-    } else if (sexo == 'Masculino' && idade >= 13 && idade <= 19) {
+    } else if (clicksexo[0].checked && age < 18) {
+        result.innerHTML = `Você é uma adolescente de ${age} anos.`
         foto.src = 'teengirl.png'
-        result.innerHTML = `Você é uma adolescente de ${idade} anos.`
-    } else if (sexo == 'Masculino' && idade >= 20 && idade < 36) {
-        foto.src = 'adultman.png'
-        result.innerHTML = `Você é um rapaz de ${idade} anos.`
-    } else if (sexo == 'Feminino' && idade >= 20 && idade < 36) {
-        result.innerHTML = `Você é uma moça de ${idade} anos.`
+    } else if (clicksexo[0].checked && age < 35) {
+        result.innerHTML = `Você é uma jovem mulher de ${age} anos.`
         foto.src = 'adultwoman.png'
-    } else if (sexo == 'Masculino' && idade >= 36 && idade < 60) {
-        result.innerHTML = `Você é um homem de meia idade de ${idade} anos.`
-        foto.src = 'middleagedman.png'
-    } else if (sexo == 'Feminino' && idade >= 36 && idade < 60) {
-        result.innerHTML = `Você é uma mulher de meia idade de ${idade} anos.`
+    } else if (clicksexo[0].checked && age < 60) {
+        result.innerHTML = `Você é uma mulher madura de ${age} anos.`
         foto.src = 'middleagedwoman.png'
-    } else if (sexo == 'Masculino' && idade >= 60) {
-        result.innerHTML = `Você é um idoso de ${idade} anos.`
+    } else if (clicksexo[0].checked && age >= 60) {
+        result.innerHTML = `Você é uma senhora de ${age} anos.`
         foto.src = 'elderly.png'
-    } else if (sexo == 'Feminino' && idade >= 60) {
-        result.innerHTML = `Você é uma idosa de ${idade} anos.`
+    } else if (clicksexo[1].checked && age < 4) {
+        result.innerHTML = `Você é um lindo bebê de ${age} aninhos.`
+        foto.src = 'baby.png'
+    } else if (clicksexo[1].checked && age < 13) {
+        result.innerHTML = `Você é um lindo menino de ${age} aninhos.`
+        foto.src = 'childboy.png'
+    } else if (clicksexo[1].checked && age < 18) {
+        result.innerHTML = `Você é um adolescente de ${age} anos.`
+        foto.src = 'teenboy.png'
+    } else if (clicksexo[1].checked && age < 35) {
+        result.innerHTML = `Você é um jovem rapaz de ${age} anos.`
+        foto.src = 'adultman.png'
+    } else if (clicksexo[1].checked && age < 60) {
+        result.innerHTML = `Você é um homem maduro de ${age} anos.`
+        foto.src = 'middleagedman.png'
+    } else if (clicksexo[1].checked && age >= 60) {
+        result.innerHTML = `Você é um senhor de ${age} anos.`
         foto.src = 'elderly.png'
-    } */
+    }
+}
